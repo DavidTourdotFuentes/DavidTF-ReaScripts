226fda641e9aef54cd2ef46fc7838fd474441054
David G4F <david@g4f.fr>
2024-08-23 12:17:08 +0200

    Bugfix Color region based on clip in region color

--- Color Management/david_Color region based on clip in region color.lua
+++ Color Management/david_Color region based on clip in region color.lua
@@ -2,7 +2,9 @@
 --@author DavidTF
 --@version 1.0
 --@changelog
---    Initial release
+--    Added buttons to jump to regions
+--    Bugfix : Default color no longer display on black color in color choices view
+--    Bugfix : Default color now properly display on color choices view
 --@about
 --    Color region based on items in the region color. If multiple colors detected, show a popup to choose witch color for each region
 
@@ -14,17 +16,21 @@ function ReturnColorsInRegion(marker_id, region_id, name, start_pos, end_pos)
 
     for i = 1, reaper.CountMediaItems(0) do
         current_item = reaper.GetMediaItem(0, i - 1)
-        item_start = reaper.GetMediaItemInfo_Value(current_item, "D_POSITION")
-        item_end = item_start + reaper.GetMediaItemInfo_Value(current_item, "D_LENGTH")
+        item_start = reaper.GetMediaItemInfo_Value(current_item, "D_POSITION") + 0.01
+        item_end = item_start + reaper.GetMediaItemInfo_Value(current_item, "D_LENGTH") - 0.01
         
         if item_start >= start_pos and item_end <= end_pos then
             item_color = reaper.GetDisplayedMediaItemColor(current_item)
             
+            -- Search to dont all color to list if color is already in the list
             found = 0
             for i=1, #color_list do
                 if color_list[i] == item_color then
                     found = found + 1
                 end
+            end
+            if item_color == 0 then
+                item_color = 4285690454
             end
             if found == 0 and item_color ~= nil then
                 table.insert(color_list, item_color)
@@ -55,14 +61,20 @@ function GuiElements()
     if reaper.ImGui_BeginTable(ctx, 'ColorTable', 1) then
     
         for i=1, #region_to_color do
+        
             reaper.ImGui_TableNextRow(ctx)
             
             reaper.ImGui_TableSetColumnIndex(ctx, 0)
             
-            
             local preview_name = region_to_color[i][2] .. " " .. region_to_color[i][3]
             
-            reaper.ImGui_Text(ctx, 'Region : ' .. preview_name)
+            if reaper.ImGui_Button(ctx, 'Region : ' .. preview_name) then
+                _, _, reg_pos, _ , _, _ = reaper.EnumProjectMarkers(region_to_color[i][1])
+                if reg_pos ~=  reaper.GetCursorPosition() then
+                  reaper.SetEditCurPos(reg_pos, true, false)
+                  reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_SELNEXTMORR'), 0)
+                end
+            end
             
             for j=4, #region_to_color[i] do
                             
@@ -71,7 +83,6 @@ function GuiElements()
                 color = (color << 8) | 0xFF -- shift 0x00RRGGBB to 0xRRGGBB00 then add 0xFF for 100% opacity
                 
                 if reaper.ImGui_ColorButton(ctx, "Color "..i*j.." ("..color..")", color, reaper.ImGui_ColorEditFlags_None(), 30.0, 30.0) then
-                    
                     reaper.Undo_BeginBlock()
                     
                     curr_retval, curr_isrgn, curr_pos, curr_rgnend, curr_name, curr_markrgnindexnumber = reaper.EnumProjectMarkers(region_to_color[i][1])
@@ -135,7 +146,6 @@ function Main()
                 if #color_list > 1 then
                     -- Let the user decide witch color choose on the marker color
                     table.insert(region_to_color, color_list)
-                    --show_colors_option()
                 else
                     -- Set the items color on the marker color
                     reaper.SetProjectMarker3(0, markrgnindexnumber, isrgn, pos, rgnend, name, color_list[1])
@@ -150,7 +160,6 @@ function Main()
                     if #color_list > 1 then
                         -- Let the user decide witch color choose on the marker color
                         table.insert(region_to_color, color_list)
-                        --show_colors_option()
                     else
                         -- Set the items color on the marker color
                         reaper.SetProjectMarker3(0, markrgnindexnumber, isrgn, pos, rgnend, name, color_list[1])
