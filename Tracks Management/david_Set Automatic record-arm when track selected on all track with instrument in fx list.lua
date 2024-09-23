8a648257b3cf37fcb8bf95339f4d3fd23c877e78
David TF <davidtourdotfuentes@gmail.com>
2024-09-23 20:09:45 +0200

    Removed debug

--- Tracks Management/david_Set Automatic record-arm when track selected on all track with instrument in fx list.lua
+++ Tracks Management/david_Set Automatic record-arm when track selected on all track with instrument in fx list.lua
@@ -1,8 +1,8 @@
 --@description Set "Automatic record-arm when track selected" on all track with instrument in fx list
 --@author DavidTF
---@version 1.0
+--@version 1.1
 --@about
---  No about.
+--  Removed debug
 
 tracks_count = reaper.CountTracks(0)
 for i=0, tracks_count - 1 do
@@ -11,7 +11,6 @@ for i=0, tracks_count - 1 do
     
     if instrument_index ~= -1 then
         _, track_name = reaper.GetTrackName(current_track)
-        reaper.ShowConsoleMsg("\n"..track_name)
         result = reaper.SetMediaTrackInfo_Value(current_track, "B_AUTO_RECARM", 1)
     end
 end
