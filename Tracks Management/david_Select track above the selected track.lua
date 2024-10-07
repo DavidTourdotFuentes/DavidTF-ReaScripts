152c185550968aceba70f34ee697eb7ad35581fe
David G4F <david@g4f.fr>
2024-10-07 15:01:31 +0200

    Fixed Select track above/below the selected track

--- Tracks Management/david_Select track above the selected track.lua
+++ Tracks Management/david_Select track above the selected track.lua
@@ -1,16 +1,17 @@
 --@description Select track above the selected track
 --@author DavidTF
---@version 1.0
---@about
---  Inital commit
+--@version 1.1
+--@changelog Bugfix: Clear selection before selecting new track
+--@about Select track below the selected track
 
 if reaper.CountSelectedTracks(0) > 0 then
-    sel_track = reaper.GetSelectedTrack(0, 0)
-    sel_track_id = reaper.GetMediaTrackInfo_Value(sel_track, 'IP_TRACKNUMBER')
+    local sel_track = reaper.GetSelectedTrack(0, 0)
+    local sel_track_id = reaper.GetMediaTrackInfo_Value(sel_track, 'IP_TRACKNUMBER')
+    
+    reaper.Main_OnCommand(40297, 0) -- Unselect (clear) selection of tracks
     
     if sel_track_id > 1 then
-        reaper.SetTrackSelected(sel_track, false)
-        new_sel_track = reaper.GetTrack(0, sel_track_id - 2)
+        local new_sel_track = reaper.GetTrack(0, sel_track_id - 2)
         reaper.SetTrackSelected(new_sel_track, true)
     end
 end
