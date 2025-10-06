78ce54fe6f3393c8e86e8836779d639091bc85ae
David G4F <david@g4f.fr>
2025-10-06 10:58:23 +0200

    Clean and activate scripts for midi items

--- Color Management/david_Color all region based on clip parent track color.lua
+++ Color Management/david_Color all region based on clip parent track color.lua
@@ -6,51 +6,47 @@
 --@about
 --    Color all regions based on clip parent track color
 
-region_to_color = {}
-colored_regions = {}
+local region_to_color = {}
 
 function ReturnColorsInRegion(marker_id, region_id, name, start_pos, end_pos)
 
     local color_list = {}
 
     for i = 1, reaper.CountMediaItems(0) do
-        current_item = reaper.GetMediaItem(0, i - 1)
-        item_start = reaper.GetMediaItemInfo_Value(current_item, "D_POSITION") + 0.01
-        item_end = item_start + reaper.GetMediaItemInfo_Value(current_item, "D_LENGTH") - 0.02
-        
-        if item_start >= start_pos and item_end <= end_pos then
-        
-            take = reaper.GetActiveTake(current_item)
-            source = reaper.GetMediaItemTake_Source(take)
-            typebuf = reaper.GetMediaSourceType(source)
+        local current_item = reaper.GetMediaItem(0, i - 1)
+        local item_start = reaper.GetMediaItemInfo_Value(current_item, "D_POSITION") + 0.01
+        local item_end = item_start + reaper.GetMediaItemInfo_Value(current_item, "D_LENGTH") - 0.02
 
-            
-            if typebuf == "WAVE" then
-                item_track = reaper.GetMediaItemTrack(current_item)
-                parent_track = reaper.GetParentTrack(item_track)
-                if parent_track then
-                    track_color = reaper.GetTrackColor(parent_track)
-                    _, name = reaper.GetTrackName(parent_track)
-                else
-                    track_color = reaper.GetTrackColor(item_track)
-                    _, name = reaper.GetTrackName(item_track)
+        if item_start >= start_pos and item_end <= end_pos then
+
+            local take = reaper.GetActiveTake(current_item)
+            local source = reaper.GetMediaItemTake_Source(take)
+
+            reaper.ShowConsoleMsg("\n"..typebuf)
+
+            local item_track = reaper.GetMediaItemTrack(current_item)
+            local parent_track = reaper.GetParentTrack(item_track)
+            local track_color
+
+            if parent_track then
+                track_color = reaper.GetTrackColor(parent_track)
+                _, name = reaper.GetTrackName(parent_track)
+            else
+                track_color = reaper.GetTrackColor(item_track)
+                _, name = reaper.GetTrackName(item_track)
+            end
+            -- Search to dont all color to list if color is already in the list
+            local found = 0
+            for i=1, #color_list do
+                if color_list[i] == track_color then
+                    found = found + 1
                 end
-                
-                --reaper.ShowConsoleMsg("\n"..name)
-                
-                -- Search to dont all color to list if color is already in the list
-                found = 0
-                for i=1, #color_list do
-                    if color_list[i] == track_color then
-                        found = found + 1
-                    end
-                end
-                if track_color == 0 then
-                    track_color = 4285690454
-                end
-                if found == 0 and track_color ~= nil then
-                    table.insert(color_list, track_color)
-                end
+            end
+            if track_color == 0 then
+                track_color = 4285690454
+            end
+            if found == 0 and track_color ~= nil then
+                table.insert(color_list, track_color)
             end
         end
     end
@@ -64,21 +60,19 @@ end
 
 function Main()
 
-    num_total, num_markers, num_regions = reaper.CountProjectMarkers(0)
-  
+    local num_total, _, _ = reaper.CountProjectMarkers(0)
+
     for i=0, num_total do
-        retval, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
-        
+        local retval, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
+
         if isrgn == true then
-            
+
             -- Find clips colors
-            color_list = ReturnColorsInRegion(i, markrgnindexnumber, name, pos, rgnend)
+            local color_list = ReturnColorsInRegion(i, markrgnindexnumber, name, pos, rgnend)
             if #color_list > 3 then
-                --reaper.ShowConsoleMsg("\n"..color_list[1].." / "..color_list[2].." / "..color_list[3].." / "..color_list[4])
-                
-                curr_retval, curr_isrgn, curr_pos, curr_rgnend, curr_name, curr_markrgnindexnumber = reaper.EnumProjectMarkers(color_list[1])
+                local _, curr_isrgn, curr_pos, curr_rgnend, curr_name, curr_markrgnindexnumber = reaper.EnumProjectMarkers(color_list[1])
                 reaper.SetProjectMarker3(0, curr_markrgnindexnumber, curr_isrgn, curr_pos, curr_rgnend, curr_name, color_list[4])
-                
+
                 -- Let the user decide witch color choose on the marker color
                 table.insert(region_to_color, color_list)
             end
