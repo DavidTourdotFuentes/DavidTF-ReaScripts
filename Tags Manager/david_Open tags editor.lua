3f08b5dc8df0ca0fb88773721026c7d3fb6f0036
David G4F <david@g4f.fr>
2026-01-19 11:57:54 +0100

    Update script dependencies

--- Tags Manager/david_Open tags editor.lua
+++ Tags Manager/david_Open tags editor.lua
@@ -16,9 +16,9 @@ dofile(reaper.GetResourcePath() ..
        '/Scripts/ReaTeam Extensions/API/imgui.lua')
   ('0.9.0.2')
 package.path = package.path..';'..debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;"
-Storage = require('Utilities/Storage')
-Gui = require('Utilities/GUI')
-Utils = require('Utilities/Utils')
+Storage = require('Utilities/TMU_Storage')
+Gui = require('Utilities/TMU_GUI')
+Utils = require('Utilities/TMU_Utils')
 
 Gui.Init()
 
